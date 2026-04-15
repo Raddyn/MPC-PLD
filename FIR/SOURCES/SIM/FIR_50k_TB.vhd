5447b61f506e1d4525cc4ff239d3d16195853dd5
Tomčala Radoslav (246958) <246958@vutbr.cz>
2026-04-15 15:09:53 +0200

    template

---
+++ FIR/SOURCES/SIM/FIR_50k_TB.vhd
@@ -0,0 +1,97 @@
+----------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+USE std.textio.ALL;
+----------------------------------------------------------------------------------
+entity FIR_50k_TB is
+end FIR_50k_TB;
+----------------------------------------------------------------------------------
+architecture tb of FIR_50k_TB is
+----------------------------------------------------------------------------------
+
+  COMPONENT FIR_50k_wrapper IS
+  GENERIC (
+    SIM_MODEL                           : BOOLEAN := TRUE
+  );
+  PORT (
+    aclk                                : IN  STD_LOGIC;
+    s_axis_data_tvalid                  : IN  STD_LOGIC;
+    s_axis_data_tready                  : OUT STD_LOGIC;
+    s_axis_data_tdata                   : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+    m_axis_data_tvalid                  : OUT STD_LOGIC;
+    m_axis_data_tdata                   : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+  );
+  END COMPONENT FIR_50k_wrapper;
+
+  -----------------------------------------------------------------------
+
+  SIGNAL sig_SIM_finished               : BOOLEAN := FALSE;     -- assert when all test vectors has been applied
+
+  CONSTANT C_aclk_period                : time := 20 ns;
+  SIGNAL aclk                           : STD_LOGIC := '0';
+
+  SIGNAL s_axis_data_tvalid             : STD_LOGIC := '0';
+  SIGNAL s_axis_data_tready             : STD_LOGIC;
+  SIGNAL s_axis_data_tdata              : STD_LOGIC_VECTOR(15 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL m_axis_data_tvalid             : STD_LOGIC;
+  SIGNAL m_axis_data_tdata              : STD_LOGIC_VECTOR(15 DOWNTO 0);
+
+----------------------------------------------------------------------------------
+begin
+----------------------------------------------------------------------------------
+
+
+  --------------------------------------------------------------------------------
+  -- Clock process definitions
+  --------------------------------------------------------------------------------
+
+  P_aclk: PROCESS
+  BEGIN
+    aclk <= '0'; WAIT FOR C_aclk_period/2;
+    aclk <= '1'; WAIT FOR C_aclk_period/2;
+    IF sig_SIM_finished THEN WAIT; END IF;
+  END PROCESS P_aclk;
+
+
+  -----------------------------------------------------------------------
+  -- Instantiate the DUT
+  -----------------------------------------------------------------------
+
+  FIR_50k_wrapper_i : FIR_50k_wrapper
+  GENERIC MAP(
+    SIM_MODEL           => TRUE
+  )
+  PORT MAP(
+    aclk                => aclk,
+    s_axis_data_tvalid  => s_axis_data_tvalid,
+    s_axis_data_tready  => s_axis_data_tready,
+    s_axis_data_tdata   => s_axis_data_tdata,
+    m_axis_data_tvalid  => m_axis_data_tvalid,
+    m_axis_data_tdata   => m_axis_data_tdata
+  );
+
+  -----------------------------------------------------------------------
+  -- FIR input
+  --    read FIR_data_in.txt
+  --    feed the FIR filter with the data from file
+  -----------------------------------------------------------------------
+
+
+
+
+
+  -----------------------------------------------------------------------
+  -- FIR output data check
+  --    read reference data from FIR_data_out.txt
+  --    compare the reference and actual data
+  --    report any discrepancy (both a text LOG file and simulator console)
+  --    report overall test result
+  -----------------------------------------------------------------------
+
+
+
+
+----------------------------------------------------------------------------------
+end tb;
+----------------------------------------------------------------------------------
